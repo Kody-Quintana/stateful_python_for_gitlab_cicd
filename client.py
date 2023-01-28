4fcd217b4ccbcfda88ff7765f4099a470c3967b6
Kody Quintana <Quintana.Kody@gmail.com>
2023-01-27 22:28:45 -0700

    Initial commit

---
+++ client.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+
+import socket
+import json
+import sys
+
+SOCKET_NAME = "/tmp/test"
+
+
+class ClientEntryPoints():
+    """Instantiate this class, then use that instance as a decorator for any functions that are to be used as an entry point."""
+    def __init__(self):
+        self.valid_entry_points = {}
+
+    def __call__(self, func):
+        self.valid_entry_points.update({func.__name__: func})
+        return func
+
+    def run(self, func_name, args):
+        """Ensure a function being requested by the server has been tagged (decorated) as a valid entry point"""
+        if func_name in self.valid_entry_points:
+            self.valid_entry_points[func_name](*args)
+        else:
+            print(f"Error {func_name} not in valid entry points", file=sys.stderr)
+            sys.exit(1)
+
+
+CLIENT_ENTRY_POINT = ClientEntryPoints()
+
+
+@CLIENT_ENTRY_POINT
+def print_server_stdout(text):
+    """Display messages from the server"""
+    if text.strip() == "":
+        return
+    for line in text.split("\n"):
+        print(f"[Daemon msg]: {line}", file=sys.stderr)
+
+
+@CLIENT_ENTRY_POINT
+def print_server_stderr(text):
+    """Display errors from the server"""
+    if text.strip() == "":
+        return
+    for line in text.split("\n"):
+        print(f"[Daemon err]: {line}", file=sys.stderr)
+
+
+@CLIENT_ENTRY_POINT
+def _exit(exit_code):
+    """Named _exit to avoid redefining builtin"""
+    sys.exit(exit_code)
+
+
+def main():  # pylint: disable=missing-function-docstring
+    if len(sys.argv) < 2:
+        print("Argument required")
+        sys.exit(1)
+
+    with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as client:
+        try:
+            client.connect(SOCKET_NAME)
+
+            # Note the trailing newline, that is there so we can use readline() in the server
+            client.sendall((json.dumps({
+                "function": sys.argv[1],
+                "args": [] if len(sys.argv) < 2 else sys.argv[2:]
+            }) + "\n").encode('utf-8'))
+
+            bufsize = 131072
+            while True:
+                message_from_server = client.recv(bufsize).strip().decode('utf-8')
+                if message_from_server:
+                    try:
+                        # Client may receive several json strings concatenated into one
+                        # so here we use raw_decode to iterate over the json strings
+                        msg_index, msg_length = 0, len(message_from_server) - 1
+                        while msg_index < msg_length:
+                            msg_object, msg_index = json.JSONDecoder().raw_decode(message_from_server, msg_index)
+
+                            CLIENT_ENTRY_POINT.run(*[msg_object.get(x) for x in ["function", "args"]])
+
+                    except json.decoder.JSONDecodeError as exception:
+                        print(exception)
+        finally:
+            client.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ server.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python3
+
+from socketserver import UnixStreamServer, StreamRequestHandler
+import traceback
+import json
+import sys
+import os
+
+
+SOCKET_NAME = "/tmp/test"
+
+
+class ServerEntryPoints():
+    """Instantiate this class, then use that instance as a decorator for any functions that are to be used as an entry point.
+    The idea is that the "server" will run any functions that are called by the client, but since the server doesn't die
+    between those calls, the state of the python script (running as the server) is maintained.
+    So for example, if one entry point instantiated a global variable, other entry points could use that variable.
+    """
+    def __init__(self):
+        self._valid_entry_points = {}
+
+    def __call__(self, func):
+        """This is called when an instance of this class is used as a decorator"""
+        self._valid_entry_points.update({func.__name__: func})
+        return func
+
+    def run(self, func_name, args):
+        """Ensure a function being requested by the client has been tagged (decorated) as a valid entry point"""
+        if func_name in self._valid_entry_points:
+            return self._valid_entry_points[func_name](*args)
+
+        error_message = f"""\"{func_name}\" not a valid entry point.
+
+Remember to first decorate those functions with @SERVER_ENTRY_POINT like this:
+
+@SERVER_ENTRY_POINT
+def my_entry_point():
+    do something here..."""
+        raise NameError(error_message)
+
+
+SERVER_ENTRY_POINT = ServerEntryPoints()
+
+
+@SERVER_ENTRY_POINT
+def set_thing(value):
+    """example entry point where we create a global"""
+    global THING  # pylint: disable=global-variable-undefined
+    THING = value
+    print(f"global variable \"thing\" is now {value}")
+
+
+@SERVER_ENTRY_POINT
+def get_thing():
+    """another example entry point where we read the new global from the first example"""
+    print(f"thing = {THING}")
+
+
+def socket_output_stream_wrapper_factory(output_stream):
+    class OutputStreamToSocket():
+        def __init__(self, stream_name):
+            self.__output_stream = output_stream
+            self.__stream_name = stream_name
+
+        def write(self, text):  # pylint: disable=missing-function-docstring
+            if len(text) == 0:
+                return
+            self.__output_stream.write(json.dumps({
+                "function": f"print_server_{self.__stream_name}",
+                "args": [text]
+            }).encode('utf-8'))
+
+        def flush(self):  # pylint: disable=missing-function-docstring
+            self.__output_stream.flush()
+    return OutputStreamToSocket
+
+
+class Handler(StreamRequestHandler):
+    """For use with ThreadedUnixStreamServer to read in messages from the client and process them"""
+
+    def tell_client_to_exit(self, exit_code=0):
+        """
+        This is used after the requested entry point function finishes so that the
+        client is blocking while the server runs the entry point function.
+        """
+        self.wfile.write(json.dumps({
+            "function": "_exit",  # Named _exit to avoid redefining builtin
+            "args": [exit_code]
+        }).encode('utf-8'))
+        self.wfile.flush()
+
+    def clean_shutdown(self, exit_value=0):
+        """Restore stdout & stderr before shutting down otherwise it will try to send a message after closing the socket"""
+        sys.stdout, sys.stderr = sys.__stdout__, sys.__stderr__
+        sys.exit(exit_value)
+
+    def handle(self):
+
+        # Send all output to client instead of having the server print it
+        OutputStreamToSocket = socket_output_stream_wrapper_factory(self.wfile)
+        sys.stdout, sys.stderr = [OutputStreamToSocket(x) for x in ['stdout', 'stderr']]
+
+        while True:  # pylint: disable=too-many-nested-blocks
+
+            # Note that for readline to work, the client sends a json string with a newline added to the end
+            message_from_client = self.rfile.readline().decode('utf-8').strip()
+
+            if message_from_client:
+                try:
+                    msg_index, msg_length = 0, len(message_from_client) - 1
+                    while msg_index < msg_length:
+                        msg_object, msg_index = json.JSONDecoder().raw_decode(message_from_client, msg_index)
+
+                        # Handle special case of shutting the server down:
+                        if msg_object.get('function') == 'exit':
+                            print(f"Shutting down {os.path.basename(__file__)}")
+                            self.tell_client_to_exit(0)
+                            self.clean_shutdown()
+                        else:
+                            try:
+                                SERVER_ENTRY_POINT.run(*[msg_object.get(x) for x in ["function", "args"]])
+                                return_value = 0
+                            except Exception:  # pylint: disable=broad-except
+                                print(traceback.format_exc(), file=sys.stderr)
+                                return_value = 1
+
+                            if return_value != 0:
+                                print(f"Shutting down {os.path.basename(__file__)}")
+                            self.tell_client_to_exit(return_value)
+                            if return_value != 0:
+                                self.clean_shutdown()
+                except Exception:  # pylint: disable=broad-except
+                    print(traceback.format_exc(), file=sys.stderr)
+                    self.tell_client_to_exit(1)
+                    self.clean_shutdown(1)
+            else:
+                return
+
+
+def main():  # pylint: disable=missing-function-docstring
+    if len(sys.argv) > 1:
+        print("Error: server takes no arguments", file=sys.stderr)
+        sys.exit(1)
+
+    try:
+        os.unlink(SOCKET_NAME)
+    except Exception:  # pylint: disable=broad-except
+        pass
+
+    with UnixStreamServer(SOCKET_NAME, Handler) as server:
+        server.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
